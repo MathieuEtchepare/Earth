85b64473fcac92bd072f6dd40d27cc1fcc1bbbe5
DylanDoAmaral <37652599+DylanDoAmaral@users.noreply.github.com>
2018-04-02 02:14:48 +0200

    Island generation

---
+++ Project/Assets/Scripts/World/Generation/PerlinNoise.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class PerlinNoise{
+    public static float[,] GenerateNoiseMap(int width, int height, int seed, float scale, int octaves, float persistance, float lacunarity)
+    {
+        float[,] noiseMap = new float[width, height];
+        System.Random prng = new System.Random(seed);
+
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for(int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 10000);
+            float offsetY = prng.Next(-100000, 10000);
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        if (scale <= 0) scale = 0.001f;
+
+        float minNoiseHeight = float.MaxValue;
+        float maxNoiseHeight = float.MinValue;
+
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for(int i = 0; i < octaves; i++)
+                {
+                    float perlinValue = CalculatePerlinNoise(x, y, scale, octaveOffsets[i].x, octaveOffsets[i].y, frequency);
+                    noiseHeight += perlinValue * amplitude;
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+
+                    
+                }
+
+                if (noiseHeight > maxNoiseHeight) maxNoiseHeight = noiseHeight;
+                else if (noiseHeight < minNoiseHeight) minNoiseHeight = noiseHeight;
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]) * CalculateIslandPelinNoise(x, y, width, height);
+            }
+        }
+
+        return noiseMap;
+    }
+
+    public static float CalculatePerlinNoise(int x, int y, float scale, float offsetX, float offsetY, float frequency)
+    {
+        float xCoord = (float)x / scale * frequency + offsetX;
+        float yCoord = (float)y / scale * frequency + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
+    }
+
+    // Add a circular mask to the perlin noise to have "round border"
+    public static float CalculateIslandPelinNoise(int x, int y, int width, int height)
+    {
+        float miWidth = width / 2;
+        float miHeight = height / 2;
+        float xCenter = x - miWidth; // Center of the circle (0;0) instead of (width/2; height/2)
+        float yCenter = y - miHeight;
+
+        float distance = Mathf.Sqrt(xCenter * xCenter + yCenter * yCenter);
+        float maxDistance = Mathf.Sqrt(miWidth * miWidth + miHeight * miHeight) - 40f;
+        float delta = distance / maxDistance;
+
+        float value = 1 - delta;
+        if (value < 0) value = 0f;
+
+        return value * 1.3f;
+    }
+
+}
---
+++ Project/Assets/Scripts/World/Generation/ProceduralIsland.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ProceduralIsland : MonoBehaviour {
+
+    public int width = 256;
+    public int height = 256;
+
+    public int seed = 1;
+    public float scale = 0.1f;
+
+    public int octaves;
+    public float persistance;
+    public float lacunarity;
+
+    public float waterFrequency = .2f;
+    public Tile[] tiles;
+    public Tilemap map;
+
+
+
+    void Start () {
+        Renderer renderer = GetComponent<Renderer>();
+        GenerateIsland();
+	}
+	
+    void GenerateIsland()
+    {
+        float[,] noiseMap = PerlinNoise.GenerateNoiseMap(width, height, seed, scale, octaves, persistance, lacunarity);
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                if(noiseMap[x,y] < waterFrequency) map.SetTile(new Vector3Int(x - width/2, y - height/2, 0), tiles[2]);
+                else if (noiseMap[x, y] < waterFrequency + 0.05f) map.SetTile(new Vector3Int(x - width / 2, y - height / 2, 0), tiles[1]);
+                else if (noiseMap[x, y] > 0.98f) map.SetTile(new Vector3Int(x - width / 2, y - height / 2, 0), tiles[4]);
+                else if (noiseMap[x, y] > 0.80f) map.SetTile(new Vector3Int(x - width / 2, y - height / 2, 0), tiles[3]);
+                else map.SetTile(new Vector3Int(x - width / 2, y - height / 2, 0), tiles[0]);
+            }
+        }
+    }
+}
