f155097a9c6ef4cf1bb03069a370439f9b82ef68
Romain Le Goas <romain.legoas@hotmail.fr>
2018-04-05 11:41:45 +0200

    Start genetic

---
+++ Project/Assets/Scripts/World/Entity/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour {
+
+    public List<Gene> appearance = new List<Gene>();
+    public List<Gene> behavior = new List<Gene>();
+    public List<Gene> composition = new List<Gene>();
+
+    protected SpriteRenderer render;
+
+    public int scale = 15;
+    private int layer = 10;
+
+    public void Start()
+    {
+        if (GetComponent<SpriteRenderer>())
+        {
+            render = GetComponent<SpriteRenderer>();
+        }
+        else
+        {
+            gameObject.AddComponent<SpriteRenderer>();
+            render = GetComponent<SpriteRenderer>();
+        }
+
+        transform.localScale = new Vector3(scale, scale, 0);
+        render.sortingOrder = layer;
+
+        Texture2D texture = GenerateTexture();
+        render.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        render.sprite.name = "Texture";
+
+    }
+
+    public abstract Texture2D GenerateTexture();
+}
