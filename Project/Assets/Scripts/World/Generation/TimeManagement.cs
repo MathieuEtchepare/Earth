e78df3c29136c67c3d967aadb3e81fb0189601c7
MathieuEtchepare <37657032+MathieuEtchepare@users.noreply.github.com>
2018-04-08 20:31:42 +0200

    Gestion du Temps
    Fonction qui fait défiler le temps en fonction des frames

---
+++ Project/Assets/Scripts/World/Generation/TimeManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManagement : MonoBehaviour
+{
+
+
+    
+    public int actual_year = 0;
+    public int actual_frame = 0;
+    int frame_limit = 200;
+    // Use this for initialization
+    void Start()
+    {
+        actual_frame = 0;
+        actual_year = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (actual_frame >= frame_limit)
+        {
+            actual_frame = 0;
+            actual_year++;
+        }
+        actual_frame++;
+    }
+}
