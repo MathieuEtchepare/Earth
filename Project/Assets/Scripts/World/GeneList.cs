e39688cb69fc80bcb4cc150ad59d8321490267a2
MathieuEtchepare <37657032+MathieuEtchepare@users.noreply.github.com>
2018-05-13 15:51:05 +0200

    Le menu d'information :D

---
+++ Project/Assets/Scripts/World/GeneList.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneList : MonoBehaviour {
+    public Text patern;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void printList(List<Gene> l)
+    {
+        Text newText;
+        newText = Instantiate(patern, transform);
+        newText.text = "Name";
+        newText.fontStyle = FontStyle.Bold;
+        newText.alignment = TextAnchor.MiddleCenter;
+        newText = Instantiate(patern, transform);
+        newText.text = "Value";
+        newText.fontStyle = FontStyle.Bold;
+        newText.alignment = TextAnchor.MiddleCenter;
+        newText = Instantiate(patern, transform);
+        newText.text = "Mutable?";
+        newText.fontStyle = FontStyle.Bold;
+        newText.alignment = TextAnchor.MiddleCenter;
+
+        for (int i = 0; i < l.Count; i++)
+        {
+            newText = Instantiate(patern, transform);
+            newText.text = l[i].name;
+            newText.alignment = TextAnchor.MiddleCenter;
+            newText = Instantiate(patern, transform);
+            newText.text = l[i].value.ToString();
+            newText.alignment = TextAnchor.MiddleCenter;
+            newText = Instantiate(patern, transform);
+            if (l[i].mutable == true) newText.text = "Yes";
+            else newText.text = "No";
+            newText.alignment = TextAnchor.MiddleCenter;
+        }
+    }
+
+    public void DestroyList(){
+        Transform transform = GetComponent<Transform>();
+        foreach (Transform child in transform)
+        {
+                Destroy(child.gameObject);
+        }
+    }
+
+    
+}
--- Project/Assets/Scripts/World/Selection.cs
+++ Project/Assets/Scripts/World/Selection.cs
@@ -5,13 +5,20 @@ using UnityEngine.UI;
 
 public class Selection : MonoBehaviour {
 
-    public Animal selected;
+    public Animal selected, prevSelected;
     public GameObject canvas;
+    public Button close;
+    public ScrollRect scroll;
+    public GeneList genes;
+    public Dropdown gene_choice;
     public Text infos;
+    private Vector3 target;
+    private string disp_selected;
 
 	// Use this for initialization
 	void Start () {
         selected = null;
+        disp_selected = null;
     }
 	
 	// Update is called once per frame
@@ -19,32 +26,59 @@ public class Selection : MonoBehaviour {
         Select();
         Window();
     }
-    
+
     void Window(){
         if (selected == null){
             canvas.SetActive(false);
         }
         else{
             canvas.SetActive(true);
-            infos.text = "Name: " + selected.name + "\n" + "Life: " + selected.life;
+            infos.text = "Name: " + selected.name + "\n" ;
+            infos.text += "Life: " + selected.currLife + " / " + selected.life + "\n";
+            infos.text += "Food: " + selected.currFood + " / " + selected.food + "\n";
+            infos.text += "Water: " + selected.currWater + " / " + selected.water + "\n";
+            infos.text += "Breath: " + selected.breath + "\n";
+            infos.text += "Speed: " + selected.speed + "\n";
+            infos.text += "Weight: " + selected.weight + "\n";
+            genes = scroll.content.GetComponent<GeneList>();
+            if (selected != prevSelected){
+                disp_selected = null;
+                prevSelected = selected;
+            }
+            if (gene_choice.captionText.text == "Appearance" && disp_selected != "Appearance"){
+                genes.DestroyList();
+                genes.printList(selected.appearance);
+                disp_selected = "Appearance";
+            }
+            else if (gene_choice.captionText.text == "Behaviour" && disp_selected != "Behaviour"){
+                genes.DestroyList();
+                genes.printList(selected.behavior);
+                disp_selected = "Behaviour";
+            }
+            else if (gene_choice.captionText.text == "Composition" && disp_selected != "Composition"){
+                genes.DestroyList();
+                genes.printList(selected.composition);
+                disp_selected = "Composition";
+            }
+            else if (gene_choice.captionText.text == "Show Genes"){
+                genes.DestroyList();
+                disp_selected = null;
+            }
         }
     }
 
     void Select(){
         if (Input.GetMouseButtonDown(0)){
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-            if (hit){
-                if (hitInfo.transform.gameObject.tag == "Entity"){
-                    selected = hitInfo.transform.gameObject.GetComponent<Animal>();
-                }
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(target.x, target.y), Vector2.zero, 0f, 1<<LayerMask.NameToLayer("Entity"));
+            if (hit) {
+                prevSelected = selected;
+                selected = hit.transform.gameObject.GetComponent<Animal>();
             }
-            else
-            {
-                selected = null;
-            }
-
         }
     }
 
+    public void Onclick(){
+        selected = null;
+    }
 }
