563d8503eb46be69794a0422067922f6175c9972
MathieuEtchepare <37657032+MathieuEtchepare@users.noreply.github.com>
2018-05-22 22:51:38 +0200

    Selection des plantes et des animaux + début barre d'infos

--- Project/Assets/Scripts/World/Selection.cs
+++ Project/Assets/Scripts/World/Selection.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class Selection : MonoBehaviour {
 
     public Entity selected, prevSelected;
-    public GameObject canvas;
+    public GameObject panel;
     public Button close;
     public ScrollRect scroll;
     public GeneList genes;
     public Dropdown gene_choice;
     public Text infos;
+    public Text year;
+    public Image o2Bar;
     private Vector3 target;
     private string disp_selected;
 
@@ -25,21 +27,34 @@ public class Selection : MonoBehaviour {
 	void Update () {
         Select();
         Window();
+        Bar();
     }
 
     void Window(){
         if (selected == null){
-            canvas.SetActive(false);
+            panel.SetActive(false);
         }
         else{
-            canvas.SetActive(true);
+            panel.SetActive(true);
             infos.text = "Name: " + selected.name + "\n" ;
-            //infos.text += "Life: " + selected.currLife + " / " + selected.life + "\n";
-            //infos.text += "Food: " + selected.currFood + " / " + selected.food + "\n";
-            //infos.text += "Water: " + selected.currWater + " / " + selected.water + "\n";
-            //infos.text += "Breath: " + selected.breath + "\n";
-            //infos.text += "Speed: " + selected.speed + "\n";
-            //infos.text += "Weight: " + selected.weight + "\n";
+            if(selected.GetComponent<MonoBehaviour>().GetType().Name == "Animal")
+            {
+                Animal a = (Animal)selected;
+                infos.text += "Life: " + a.currLife + " / " + a.life + "\n";
+                infos.text += "Food: " + a.currFood + " / " + a.food + "\n";
+                infos.text += "Water: " + a.currWater + " / " + a.water + "\n";
+                infos.text += "Breath: " + a.breath + "\n";
+                infos.text += "Speed: " + a.speed + "\n";
+                infos.text += "Weight: " + a.weight + "\n";
+                infos.text += "Age: " + a.age + "\n";
+            }
+            else if (selected.GetComponent<MonoBehaviour>().GetType().Name == "Flower")
+            {
+                Flower f = (Flower)selected;
+                infos.text += "Life: " + f.currLife + " / " + f.life + "\n";
+                infos.text += "Age: " + f.age + "\n";
+            }
+
             genes = scroll.content.GetComponent<GeneList>();
             if (selected != prevSelected){
                 disp_selected = null;
@@ -71,11 +86,22 @@ public class Selection : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)){
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(new Vector2(target.x, target.y), Vector2.zero, 0f, 1<<LayerMask.NameToLayer("Entity"));
-            if (hit) {
+            RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(target.x, target.y), Vector2.zero, 0f, 1 << LayerMask.NameToLayer("Flower"));
+            if (hit || hit2) {
                 prevSelected = selected;
-                selected = hit.transform.gameObject.GetComponent<Entity>();
+                if (hit) selected = hit.transform.gameObject.GetComponent<Animal>();
+                else selected = hit2.transform.gameObject.GetComponent<Flower>();
+                
             }
         }
+    }
+
+    void Bar()
+    {
+        year.text = "Year: " + ProceduralIsland.instance.GetComponent<TimeManagement>().actual_year.ToString();
+        float o2 = ProceduralIsland.instance.GetComponent<Atmosphere>().oxygene;
+        float co2 = ProceduralIsland.instance.GetComponent<Atmosphere>().co2;
+        o2Bar.fillAmount = (o2 - co2) / (o2 + co2);
     }
 
     public void Onclick(){
